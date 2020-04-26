4fc11a849cb99963c9d9bd18f9c8311172f5f47a
aleksileinonen <46646373+aleksileinonen@users.noreply.github.com>
2020-04-26 21:06:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/gpslocation1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.gpslocation1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.text.DecimalFormat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FusedLocationProviderClient fusedLocationClient;
+    TextView textView;
+    private int locationRequestCode = 1000;
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    locationRequestCode);
+
+        } else {
+
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                textView = findViewById(R.id.locationText);
+                                double locLongitude = location.getLongitude();
+                                double locLatitude = location.getLatitude();
+                                String strLongitude = (df.format(locLongitude));
+                                String strLatitude = (df.format(locLatitude));
+                                textView.setText(strLatitude + ". " + strLongitude);
+
+
+                            }
+
+                     }
+             });
+        }
+    }
+}
+
